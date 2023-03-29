543c87745df984cd8d0581fe1c70c65a6202b6ec
Preetham N <preethamn444@gmail.com>
2023-03-29 12:21:41 +0530

    changes

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div className='navs'>
+                        <a href = "https://javaguides.net" className = "navbar-brand">
+                            EMPLOYEE MANAGEMENT APPLICATION
+                        </a>
+                    </div>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
